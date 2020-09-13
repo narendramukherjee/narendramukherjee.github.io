174063fc8d9fb4f54fdc08e286b9a5bc6be06488
narendramukherjee <narendra.mukherjee@gmail.com>
2020-09-13 19:00:38 -0400

    Nikola auto commit.
    Nikola version: 8.0.3

--- cache/.mako.tmp/index_helper.tmpl.py
+++ cache/.mako.tmp/index_helper.tmpl.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1600037833.199684
+_modified_time = 1600038038.313099
 _enable_loop = True
 _template_filename = '/home/narendra/anaconda3/envs/website/lib/python3.8/site-packages/nikola/data/themes/bootstrap4/templates/index_helper.tmpl'
 _template_uri = 'index_helper.tmpl'
@@ -27,8 +27,8 @@ def render_body(context,**pageargs):
 def render_html_pager(context):
     __M_caller = context.caller_stack._push_frame()
     try:
-        prevlink = context.get('prevlink', UNDEFINED)
         messages = context.get('messages', UNDEFINED)
+        prevlink = context.get('prevlink', UNDEFINED)
         nextlink = context.get('nextlink', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n')
